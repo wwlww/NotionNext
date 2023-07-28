c21460679c2fec19c02aed72fd7cfd7c5d664133
wenlong wang <72427323+wwlww@users.noreply.github.com>
2023-07-28 16:15:00 +0800

    Update custom.js
    add dify

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,9 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+<script>
+ window.difyChatbotConfig = { token: 'tKX2pZfbSxWE3kV2' }
+</script>
+<script
+ src="https://udify.app/embed.min.js"
+ id="tKX2pZfbSxWE3kV2"
+ defer>
+</script>
