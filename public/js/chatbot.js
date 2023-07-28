f9eac9ae259645ee42afe95dfc368f849a74828c
wenlong wang <72427323+wwlww@users.noreply.github.com>
2023-07-28 16:24:32 +0800

    Create chatbot.js
    add chatbot.js

---
+++ public/js/chatbot.js
@@ -0,0 +1,12 @@
+// chatbot.js
+
+const chatbot = `
+  <iframe 
+    src="https://udify.app/chatbot/tKX2pZfbSxWE3kV2" 
+    style="width: 300px; height: 500px;"
+  ></iframe>
+`
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.body.innerHTML += chatbot
+})
